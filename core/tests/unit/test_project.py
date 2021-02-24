357c286c713a7afe42431b2490aa7b178ad21924
kad1n <thekad1nat@gmail.com>
2021-02-24 12:32:06 +0100

    feat: add test for remove job in project

--- core/tests/unit/test_project.py
+++ core/tests/unit/test_project.py
@@ -4,12 +4,12 @@ from core.model import Job, Project
 
 
 @pytest.fixture
-def make_test_project():
+def make_test_project() -> Project:
     project = Project("Test name", "Test description")
     project.add_job(Job("Test job name 1"))
     project.add_job(Job("Test job name 2"))
     project.add_job(Job("Test job name 3"))
-    yield project
+    return project
 
 
 def test_make_project_and_add_job(make_test_project):
@@ -26,3 +26,17 @@ def test_make_project_get_jobs(make_test_project):
 
     assert isinstance(jobs, list)
     assert isinstance(jobs[0], Job)
+
+
+def test_remove_job(make_test_project):
+    project = make_test_project
+    jobs = project.get_jobs()
+
+    assert project.remove_job(jobs[1]) == True
+    assert len(project.get_jobs()) == 2
+    assert project.remove_job(jobs[1]) == False
+    assert len(project.get_jobs()) == 2
+
+    project.add_job(jobs[1])
+
+    assert len(project.get_jobs()) == 3
