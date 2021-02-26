8acc6e09ec3ea91bdbc1698a3f555616ddf34009
kad1n <thekad1nat@gmail.com>
2021-02-26 12:26:30 +0100

    refactor: remove `run()` in core main

--- core/core/main.py
+++ core/core/main.py
@@ -30,24 +30,7 @@ def main(argsv: Optional[Sequence[str]] = None) -> int:
         logging.basicConfig(level=logging.INFO)
         logging.info("Core started")
 
-    # run()
-
     return 0
-
-
-def run() -> None:
-    # Testing setup of repository.
-    engine = create_engine("sqlite:///data.db", echo=True)
-    metadata.create_all(engine)
-    session_maker = sessionmaker(bind=engine)
-    start_mappers()
-
-    """Test run application."""
-    logger.debug("Making repo")
-    project_repo = ProjectRepository(session_maker())
-    new_project = model.Project("test", "1", "test beskivelse")
-    project_repo.add(new_project)
-    print(list(map(lambda p: print(p.__dict__), project_repo.list())))
 
 
 if __name__ == "__main__":
