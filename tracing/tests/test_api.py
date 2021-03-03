42cefc718283c339d293afcdfeb6982b41b77ec0
Birger J. Nordølum <contact@mindtooth.no>
2021-03-03 10:22:42 +0100

    style: fix imports

--- tracing/tests/test_api.py
+++ tracing/tests/test_api.py
@@ -2,6 +2,7 @@
 
 import pytest
 from fastapi.testclient import TestClient
+
 from tracing.api import tracking
 
 
--- tracing/tests/test_object.py
+++ tracing/tests/test_object.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import pytest
+
 from tracing.tracker import BBox, Detection, Object
 
 
