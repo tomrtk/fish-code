d6b9f7afe831413d67c443ae48775870deaf2c52
Tom-R Kvalvaag <trkvalva@stud.ntnu.no>
2021-03-11 09:15:14 +0000

    fix: ignores types in run.py
    to make pyright pass. There lacks a lot of typehinting in the UI module
    but that is outside the scope of this MR.

---
+++ run.py
@@ -0,0 +1,61 @@
+import argparse
+import logging
+from multiprocessing import Process
+from typing import List, Optional, Sequence
+
+from app.run import serve_debug, serve_prod  # type: ignore
+
+from core.main import main as core_main  # type: ignore
+from tracing.main import main as tracing_main  # type: ignore
+
+logger = logging.getLogger(__name__)
+
+
+def main(argsv: Optional[Sequence[str]] = None) -> int:
+    """Run all packages in parallel from root project.
+
+    Parameters
+    ----------
+    argsv   :   Optional[Sequence[str]]
+                Command line arguments if functions is called directly.
+    """
+    main_parser = argparse.ArgumentParser()
+    main_parser.add_argument(
+        "--dev",
+        default=False,
+        action="store_true",
+        help="Run web server in development mode.",
+    )
+    main_args = main_parser.parse_args(argsv)
+    processes: List[Process] = list()
+
+    if not main_args.dev:
+        ui_process = Process(target=serve_prod)
+    else:
+        ui_process = Process(target=serve_debug)
+
+    ui_process.daemon = True
+    processes.append(ui_process)
+
+    core_process = Process(target=core_main)
+    core_process.daemon = True
+    processes.append(core_process)
+
+    tracing_process = Process(target=tracing_main)
+    tracing_process.daemon = True
+    processes.append(tracing_process)
+
+    # Start all processes
+    for p in processes:
+        logger.info("Starting process %s", p.name)
+        p.start()
+
+    # Wait for processes to close
+    for p in processes:
+        p.join()
+
+    return 0
+
+
+if __name__ == "__main__":
+    exit(main())
