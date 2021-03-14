4488abe79a54c7e26f790d9ac80a3da88d6f387e
Eirik Osland Lavik <eiriklav@stud.ntnu.no>
2021-03-14 18:50:03 +0100

    fix(tracing): small fixup in main

--- tracing/tracing/main.py
+++ tracing/tracing/main.py
@@ -1,9 +1,13 @@
 """Entrypoint to start API."""
 import uvicorn
 
-from . import api
+from tracing import api
 
 
 def main():
     """Entrypoint to start API."""
     uvicorn.run(api.tracking, host="0.0.0.0", port=8001)
+
+
+if __name__ == "__main__":
+    main()
