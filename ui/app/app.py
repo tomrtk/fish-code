f2771fbad1986e00f4d6bb6ef7f885bbd784c4f4
Birger J. Nordølum <contact@mindtooth.no>
2021-03-15 17:55:12 +0100

    chore: more stuff

--- ui/app/app.py
+++ ui/app/app.py
@@ -5,32 +5,29 @@ import os
 from flask import Flask, render_template
 
 from app.model import *
-from app.projects.projects import projects_bp
+from app.projects.projects import construct_projects_bp
 
 
 def create_app(test_config=None):  # type: ignore
     """Application factory to setup the loading of the application."""
-    app = Flask(__name__, instance_relative_config=True)
-
-    app.config.from_mapping(  # type: ignore
-        SECRET_KEY="dev",
-        BACKEND_URL="http://127.0.0.1:9955",
-    )
-
-    # app.config["TEMPLATES_AUTO_RELOAD"] = True
+    app = Flask(__name__, instance_relative_config=False)
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
-        app.config.from_pyfile("config.py", silent=True)  # type: ignore
+        app.config.from_pyfile("config.cfg", silent=False)  # type: ignore
     else:
         # load the test config if passed in
         app.config.from_mapping(test_config)  # type: ignore
+
+    print(app.config["BACKEND_URL"])
 
     # ensure the instance folder exists
     try:
         os.makedirs(app.instance_path)
     except OSError:
         pass
+
+    projects_bp = construct_projects_bp(app.config)
 
     app.register_blueprint(projects_bp, url_prefix="/projects")
 
--- ui/app/projects/projects.py
+++ ui/app/projects/projects.py
@@ -1,109 +1,130 @@
 """Blueprint for the projects module."""
 import json
 import os
+from dataclasses import asdict
 
-from flask import Blueprint, redirect, render_template, request, url_for
+import requests
+from flask import Blueprint, Config, redirect, render_template, request, url_for
 
-from app.model import Detection, Video
-
-projects_bp = Blueprint(
-    "projects_bp",
-    __name__,
-    template_folder="templates",
-    static_folder="static",
-)
+from app.model import Detection, Project, Video
 
 
-@projects_bp.route("/")
-def projects_index():
-    """Entrypoint for the blueprint."""
-    return render_template("projects/index.html")
-
-
-@projects_bp.route("/new")
-def projects_project_new():
-    """Create a new project."""
-    return render_template("projects/project_new.html")
-
-
-@projects_bp.route("/<int:project_id>")
-def projects_project(project_id: int):
-    """View a single project."""
-    return render_template("projects/project.html")
-
-
-@projects_bp.route("/<int:project_id>/jobs/<int:job_id>")
-def projects_job(project_id: int, job_id: int):
-    """View a single job."""
-    detections = [
-        Detection(
-            **{
-                "id": i,
-                "report_type": f"Type{i}",
-                "start": "Now",
-                "stop": "Later",
-                "video_path": "C:\\",
-            }
-        )
-        for i in range(1, 100)
-    ]
-    videos = [
-        Video(
-            **{
-                "id": i,
-                "location": f"Test{i}",
-                "status": "{status}".format(
-                    status="Pendig" if (i % 2) == 0 else "Ferdig"
-                ),
-                "video_path": f"C:\\Filmer\\Film_{i*2}",
-            }
-        )
-        for i in range(1, 101)
-    ]
-
-    return render_template(
-        "projects/job.html", detections=detections, videos=videos
+def construct_projects_bp(cfg: Config):
+    """Create constructor from function to pass in config."""
+    projects_bp = Blueprint(
+        "projects_bp",
+        __name__,
+        template_folder="templates",
+        static_folder="static",
     )
+
+    @projects_bp.route("/")
+    def projects_index():
+        """Entrypoint for the blueprint."""
+        return render_template("projects/index.html")
+
+    @projects_bp.route("/new", methods=["POST", "GET"])
+    def projects_project_new():
+        """Create a new project."""
+        if request.method == "POST":
+            project = Project(
+                **{
+                    "name": request.form["project_name"],
+                    "number": request.form["project_id"],
+                    "description": request.form["project_desc"],
+                }
+            )
+
+            post_project(project, cfg["BACKEND_URL"])
+
+            return redirect(url_for("projects_bp.projects_index"))
+
+        return render_template("projects/project_new.html")
+
+    @projects_bp.route("/<int:project_id>")
+    def projects_project(project_id: int):
+        """View a single project."""
+        return render_template("projects/project.html")
+
+    @projects_bp.route("/<int:project_id>/jobs/<int:job_id>")
+    def projects_job(project_id: int, job_id: int):
+        """View a single job."""
+        detections = [
+            Detection(
+                **{
+                    "id": i,
+                    "report_type": f"Type{i}",
+                    "start": "Now",
+                    "stop": "Later",
+                    "video_path": "C:\\",
+                }
+            )
+            for i in range(1, 100)
+        ]
+        videos = [
+            Video(
+                **{
+                    "id": i,
+                    "location": f"Test{i}",
+                    "status": "{status}".format(
+                        status="Pendig" if (i % 2) == 0 else "Ferdig"
+                    ),
+                    "video_path": f"C:\\Filmer\\Film_{i*2}",
+                }
+            )
+            for i in range(1, 101)
+        ]
+
+        return render_template(
+            "projects/job.html", detections=detections, videos=videos
+        )
+
+    @projects_bp.route("/<int:project_id>/jobs/new", methods=["POST", "GET"])
+    def projects_job_new(project_id: int):
+        """Create new job inside a project."""
+        data = path_to_dict("/Users/mt/Downloads")
+
+        if request.method == "POST":
+            print(request.form)
+            return redirect(
+                url_for("projects_bp.projects_project", project_id=project_id)
+            )
+
+        return render_template(
+            "projects/job_new.html",
+        )
+
+    @projects_bp.route("/json")
+    def projects_json():
+        """Create new job inside a project."""
+        data = path_to_dict("/Users/mt/Downloads")
+
+        return data
+
+    @projects_bp.route("/jobs")
+    def projects_jobs():
+        """Route for serving a large table."""
+        return render_template("projects/report/result.html")
+
+    return projects_bp
+
+
+def post_project(project: Project, endpoint: str):
+    """Post new project to endpoint."""
+    r = requests.post(endpoint + "/projects/", data=project.to_json())
+
+    if not r.status_code == requests.codes.ok:
+        print("nei")
+
+    return redirect(url_for("projects_bp.projects_index"))
 
 
 def path_to_dict(path):
     """Polute endpoint with stuff."""
     d = {"text": os.path.basename(path)}
     if os.path.isdir(path):
-        d["type"] = "directory"
         d["children"] = [
             path_to_dict(os.path.join(path, x)) for x in os.listdir(path)
         ]
-    else:
-        d["type"] = "file"
+
     return d
-
-
-@projects_bp.route("/<int:project_id>/jobs/new", methods=["POST", "GET"])
-def projects_job_new(project_id: int):
-    """Create new job inside a project."""
-    data = path_to_dict("/Users/mt/Downloads")
-
-    if request.method == "POST":
-        print(request.form)
-        return redirect(
-            url_for("projects_bp.projects_project", project_id=project_id)
-        )
-
-    return render_template(
-        "projects/job_new.html",
-    )
-
-
-@projects_bp.route("/json")
-def projects_json():
-    """Create new job inside a project."""
-    data = path_to_dict("/Users/mt/Downloads")
-
-    return data
-
-
-@projects_bp.route("/jobs")
-def projects_jobs():
-    """Route for serving a large table."""
-    return render_template("projects/report/result.html")
