095c69537cbb16d5b996466788e82942b8fda21e
Eirik Osland Lavik <eiriklav@stud.ntnu.no>
2021-03-23 13:57:01 +0100

    feat(core::model): to_json for detection and object
    Had issues converting these dataclasses to json. This commit solves this
    problem.

--- core/core/model.py
+++ core/core/model.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 import logging
 import os.path
 import re
-from dataclasses import dataclass
+from dataclasses import asdict, dataclass
 from datetime import datetime, timedelta
 from enum import Enum
 from pathlib import Path
@@ -371,6 +371,28 @@ class Frame:
     detections: List[Detection]
     timestamp: Optional[datetime] = None
 
+    def to_json(self) -> Dict[str, Any]:
+        """Convert frame to json.
+
+        Return
+        ------
+        Dict[str, Any] :
+            Object as json:
+            {
+                "idx": int,
+                "detections": List[Detection],
+                "timestamp": None|str
+            }
+
+        """
+        return {
+            "idx": self.idx,
+            "detections": [det.to_json() for det in self.detections if det],
+            "timestamp": None
+            if not self.timestamp
+            else self.timestamp.isoformat(),
+        }
+
 
 @dataclass
 class BBox:
@@ -409,6 +431,46 @@ class Detection:
     probability: float
     label: int
     frame: int
+
+    def to_json(self) -> Dict[str, Any]:
+        """Convert to json.
+
+        Return
+        ------
+        Dict[str, Any] :
+            Detection as json,
+            {
+                "bbox": BBox,
+                "probability": float,
+                "label": int,
+                "frame": int,
+            }
+
+        """
+        return {
+            "bbox": asdict(self.bbox),
+            "probability": self.probability,
+            "label": self.label,
+            "frame": self.frame,
+        }
+
+    def set_frame(self, frame: int) -> Detection:
+        """update frame nr.
+
+        Returns itself so it can be used in list comprehensions.
+
+        Parameter
+        ---------
+        frame : int
+              The frame number the detection is found in.
+
+        Return
+        ------
+        Detection :
+                  Returns self.
+        """
+        self.frame = frame
+        return self
 
     @classmethod
     def from_api(
