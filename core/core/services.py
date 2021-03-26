3162b807ab5ec7c874b60aa66b367027fe28e5ac
kad1n <thekad1nat@gmail.com>
2021-03-26 15:17:23 +0100

    feat: move scheduler to services

--- core/core/services.py
+++ core/core/services.py
@@ -1,6 +1,8 @@
 """Services used in this application."""
 import logging
+import multiprocessing
 from datetime import datetime
+from typing import List
 
 import numpy as np
 
@@ -9,7 +11,66 @@ from core.model import Job, Video
 
 logger = logging.getLogger(__name__)
 
-from typing import List
+
+class Worker(multiprocessing.Process):
+    """Worker to process a job."""
+
+    def __init__(self, task_queue, result_queue):
+        multiprocessing.Process.__init__(self)
+        self.task_queue = task_queue
+        self.result_queue = result_queue
+
+    def run(self):
+        """Worker thread core loop."""
+        while True:
+            next_task = self.task_queue.get()
+            if next_task is None:
+                logger.info(f"Process {self.name} exiting, no more work to do.")
+                self.task_queue.task_done()
+                break
+            elif isinstance(next_task, Job):
+                logger.info(
+                    f"Process {self.name} received job '{next_task.name}'"
+                )
+                processed_job = process_job(next_task)
+                self.result_queue.put(processed_job)
+                self.task_queue.task_done()
+        return
+
+
+class Singleton(type):
+    """Singleton class definition, there can only be one instance."""
+
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        """Ensure there is only once instance, and same instance returned if created."""
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(
+                *args, **kwargs
+            )
+        return cls._instances[cls]
+
+
+class Scheduler(metaclass=Singleton):
+    """Scheduler class to control processing of jobs."""
+
+    def __init__(self) -> None:
+        self.tasks = multiprocessing.JoinableQueue()
+        self.results = multiprocessing.Queue()
+
+        logger.info("Creating 2 job workers")
+        workers = [Worker(self.tasks, self.results) for i in range(2)]
+        for w in workers:
+            w.start()
+
+    async def put_job(self, job: Job):
+        """Put a job into the scheduling queue."""
+        logger.info(f"putting job {job.name} in queue")
+        self.tasks.put(job)
+
+
+scheduler = Scheduler()
 
 
 class VideoLoader:
