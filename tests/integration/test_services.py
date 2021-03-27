2c7e53704df32779c94808d45219dc519c41d6ce
kad1n <thekad1nat@gmail.com>
2021-03-27 17:43:33 +0100

    test(services): test scheduler functionality

--- tests/integration/test_services.py
+++ tests/integration/test_services.py
@@ -2,31 +2,44 @@
 import logging
 
 import pytest
-from core.services import process_job, VideoLoader
+import time
+import core.services as services
 
-from core.model import Job, Video, Status
-from core.services import process_job
-from datetime import datetime
+from core.model import Project, Job, Video
 
 logger = logging.getLogger(__name__)
 
 
-@pytest.mark.usefixtures("tracing_api", "detection_api")
-def test_job_processing():
+def test_scheduler():
     """Test the processing of a job."""
+    project = Project("Project name", "NINA-123", "Project description")
+    project.id = 50000
     job = Job("Test Name", "Test Description", "Test Location")
+    job.id = 50000
+    project.add_job(job)
     vid1 = Video.from_path(
         "./tests/integration/test_data/test-abbor[2021-01-01_00-00-00]-000-small.mp4"
     )
     job.add_videos([vid1])
     job.start()
-    finished_job = process_job(job)
-    assert len(finished_job._objects) > 0
-    for o in finished_job._objects:
-        assert isinstance(o.time_in, datetime)
-        assert isinstance(o.time_out, datetime)
-        assert o.time_in <= o.time_out
-    assert finished_job.status() == Status.DONE
+
+    # Check that the scheduling thread starts and queue
+    services.start_scheduler()
+    assert services.schedule_thread.is_alive()
+    assert services.job_queue.qsize() == 0
+
+    # Insert job to scheduler
+    services.queue_job(project.id, job.id)
+    assert services.job_queue.qsize() == 1
+
+    # Check that the job gets consumed
+    time.sleep(1)
+    assert services.job_queue.qsize() == 0
+
+    # Ensure scheduler stops
+    services.stop_scheduler()
+    time.sleep(1)
+    assert not services.schedule_thread.is_alive()
 
 
 def test_video_loader():
@@ -37,7 +50,7 @@ def test_video_loader():
         ),
     ]
 
-    video_loader = VideoLoader(videos, 15)
+    video_loader = services.VideoLoader(videos, 15)
 
     results = []
     for batch, _ in video_loader:
