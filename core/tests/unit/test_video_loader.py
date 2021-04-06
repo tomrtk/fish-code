82d17b8884ca3baddfca4539fb1b4da88cba81e4
Eirik Osland Lavik <eiriklav@stud.ntnu.no>
2021-04-06 15:05:50 +0200

    feat(test::video_loader): tests new functionality
    Checks relative frames, videos, timestamps and batch.

--- core/tests/unit/test_video_loader.py
+++ core/tests/unit/test_video_loader.py
@@ -1,7 +1,10 @@
+"""Unit test of video loader."""
 import logging
+from datetime import datetime, timedelta
+
+import pytest
 
 import core.services as services
-
 from core.model import Video
 
 logger = logging.getLogger(__name__)
@@ -9,18 +12,51 @@ logger = logging.getLogger(__name__)
 
 def test_video_loader():
     """Tests the video loader utility class."""
+    vid1_path = "./tests/unit/test-[2020-03-28_12-30-10]-000.mp4"
+    vid2_path = "./tests/unit/test-[2020-03-28_12-30-10]-001.mp4"
     videos = [
-        Video.from_path("./tests/unit/test-[2020-03-28_12-30-10].mp4"),
+        Video.from_path(vid1_path),
+        Video.from_path(vid2_path),
     ]
 
     video_loader = services.VideoLoader(videos, 15)
+    fps = video_loader.videos[0].fps
 
-    results = []
-    for batch, _, _, _ in video_loader:
-        results.append(len(batch))
+    batch_len = []
+    timestamps = []
+    rel_frame = []
+    videos = []
+    for batch, times, rel_f, vids in video_loader:
+        batch_len.append(len(batch))
+        [timestamps.append(t) for t in times]
+        [rel_frame.append(f) for f in rel_f]
+        [videos.append(v) for v in vids]
 
-    assert results[0] == 15  # batch size == 15
-    assert results[2] == 15  # Keep using max batch size where it can
-    assert results[3] == 15
-    assert results[4] == 10  # Final 10 frames in the 50 frame video
-    assert len(results) == 5  # total num batches
+    # Testing batch lengths
+    assert batch_len[0] == 15  # batch size == 15
+    assert batch_len[2] == 15  # Keep using max batch size where it can
+    assert batch_len[-1] == 5  # Final 5 frames in the 140 frame "video"
+    assert len(batch_len) == 10  # total num batches
+
+    # Testing timestamps
+    assert timestamps[0] == datetime(2020, 3, 28, 12, 30, 10)
+
+    random_frame = 40  # "random" in first video
+    assert timestamps[random_frame] == datetime(
+        2020, 3, 28, 12, 30, 10
+    ) + timedelta(seconds=int(random_frame * (1 / fps)))
+
+    # Testing relative frames
+    assert rel_frame[0] == 0  # First frame first video
+    assert rel_frame[69] == 69  # last frame first video
+    assert rel_frame[70] == 0  # First frame second video
+    assert rel_frame[139] == 69  # last frame second video
+    with pytest.raises(IndexError):
+        _ = rel_frame[140]
+
+    # Testing videos
+    assert len(videos) == 140
+    assert videos[0]._path == vid1_path
+    assert videos[69]._path == vid1_path
+    assert videos[70]._path == vid2_path
+    assert videos[139]._path == vid2_path
