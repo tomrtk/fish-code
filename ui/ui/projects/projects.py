ded2edfc7921b370c34e3d2a15b63d1e94336fde
Birger J. Nordølum <contact@mindtooth.no>
2021-04-13 10:31:09 +0200

    refactor(ui): convert api to module

--- ui/ui/projects/projects.py
+++ ui/ui/projects/projects.py
@@ -1,11 +1,9 @@
 """Blueprint for the projects module."""
-import functools
 import logging
 import os
 import tempfile
-from typing import Any, Callable, Dict, List, Optional, Tuple, TypeVar
+from typing import Any, Dict
 
-import requests
 from flask import (
     Blueprint,
     Config,
@@ -15,141 +13,16 @@ from flask import (
     render_template,
     request,
     send_file,
-    session,
     url_for,
 )
 
-from ui.model import Detection, Job, Project, Video
+from ui.projects.api import Client
+from ui.projects.model import Job, Project
 
 logger = logging.getLogger(__name__)
 logger.level = logging.DEBUG
 
 root_folder = "~/Downloads"
-
-T = TypeVar("T")
-Result = Optional[T]
-
-
-class Client:
-    """API client class.
-
-    Wrapping the error handling of `requests` calls to api by using the
-    `API.call` decorator.
-
-    Attributes
-    ----------
-    _session    :   requests.Session
-                    Keeps Session between calls.
-
-    Methods
-    -------
-    get(uri: str)
-        Perform a GET request to `uri`.
-    post(uri: str, data: Dict[str, Any])
-        Perform a POST request to `uri` with `data` as body of request.
-
-
-    Examples
-    --------
-    >>> client = Client()
-    >>> response = client.get("http://127.0.0.1:8000/projects/")
-    """
-
-    def __init__(self) -> None:
-        self._session: requests.Session = requests.Session()
-
-    class Api:
-        """Api decorator class."""
-
-        @classmethod
-        def call(
-            csl, request: Callable = None, *, status_code: int = 200
-        ) -> Callable:
-            """Handle errors for api call.
-
-            Used as decorator on `Client` methods calling HTTP Methods to API.
-
-            Parameters
-            ----------
-            request     :   Callable
-                            Function calling API.
-            status_code :   int
-                            Success status_code from API.
-            """
-
-            def decorator_call(request):
-                functools.wraps(request)
-
-                def wrapper_call(*args, **kwargs) -> Result[requests.Response]:
-                    try:
-                        response = request(*args, **kwargs)  # type: ignore
-                    except requests.ConnectionError as e:
-                        logger.warning(
-                            "ConnectionError:",
-                            request.__name__,
-                            request.__dict__,
-                            e,
-                        )
-                        return None
-
-                    if not response.status_code == status_code:
-                        logger.warning(
-                            "Status code: %s, error: %s",
-                            response.status_code,
-                            response.json().__dict__,
-                        )
-                        return None
-                    return response
-
-                return wrapper_call
-
-            if request is None:
-                return decorator_call
-            else:
-                return decorator_call(request)
-
-    @Api.call(status_code=200)
-    def get(self, uri: str) -> requests.Response:
-        """Perform a GET request to `uri.
-
-        Expects a successful call to return a status_code = 200.
-
-        Parameters
-        ----------
-        uri     :   str
-                    API endpoint.
-
-        Returns
-        -------
-        Optional[requests.Response]
-            Returns a `Response` if no errors, else `None`
-        """
-        logger.info("get from %s", uri)
-        return self._session.get(uri)
-
-    @Api.call(status_code=200)
-    def post(self, uri: str, data: Dict[str, Any]) -> requests.Response:
-        """Perform a POST request to `uri` with `data` as body of request.
-
-        Expects a successful call to return a status_code = 200.
-
-        Parameters
-        ----------
-        uri     :   str
-                    API endpoint.
-        data    :   Dict[str, Any]
-                    Data to be posted as request body.
-
-        Returns
-        -------
-        Optional[requests.Response]
-            Returns a `Response` if no errors, else `None`
-        """
-        logger.info("post to %s, %s", uri, data)
-        return self._session.post(uri, data=data)
-
-
-handler = Client()
 
 
 def construct_projects_bp(cfg: Config):
@@ -161,15 +34,16 @@ def construct_projects_bp(cfg: Config):
         static_folder="static",
     )
 
-    endpoint_path = cfg["BACKEND_URL"]
+    endpoint_path: str = cfg["BACKEND_URL"]
+    client = Client(endpoint_path)
 
     @projects_bp.route("/")
     def projects_index():  # type: ignore
         """Entrypoint for the blueprint."""
-        if not check_api(endpoint_path):
+        if not client.check_api():
             return render_template("api_down.html")
 
-        projects = get_projects(endpoint_path)
+        projects = client.get_projects()
         return render_template("projects/projects.html", projects=projects)
 
     @projects_bp.route("/new", methods=["POST", "GET"])
@@ -185,7 +59,7 @@ def construct_projects_bp(cfg: Config):
                 }
             )
 
-            post_project(project, endpoint_path)
+            client.post_project(project)
 
             return redirect(url_for("projects_bp.projects_index"))
 
@@ -194,7 +68,7 @@ def construct_projects_bp(cfg: Config):
     @projects_bp.route("/<int:project_id>")
     def projects_project(project_id: int):  # type: ignore
         """View a single project."""
-        project = get_project(project_id, endpoint_path)
+        project = client.get_project(project_id)
         if not project:
             abort(404)
 
@@ -203,7 +77,7 @@ def construct_projects_bp(cfg: Config):
     @projects_bp.route("/<int:project_id>/jobs/<int:job_id>")
     def projects_job(project_id: int, job_id: int):  # type: ignore
         """View a single job."""
-        job = get_job(job_id, project_id, endpoint_path)
+        job = client.get_job(project_id, job_id)
         obj_stats = job.get_object_stats()
 
         return render_template(
@@ -215,16 +89,17 @@ def construct_projects_bp(cfg: Config):
     )  # type: ignore
     def projects_job_toggle(project_id: int, job_id: int):  # type: ignore
         """Toogle job status."""
-        old_status, new_status = change_job_status(
-            project_id, job_id, endpoint_path
-        )
+        old_status, new_status = client.change_job_status(project_id, job_id)
+
+        if old_status is None or new_status is None:
+            return 404
 
         return jsonify(old_status=old_status, new_status=new_status), 201  # type: ignore
 
     @projects_bp.route("/<int:project_id>/jobs/new", methods=["POST", "GET"])
     def projects_job_new(project_id: int):  # type: ignore
         """Create new job inside a project."""
-        project = get_project(project_id, endpoint_path)
+        project = client.get_project(project_id)
 
         if request.method == "POST":
             hardcoded_path = os.path.dirname(os.path.expanduser(root_folder))
@@ -250,7 +125,7 @@ def construct_projects_bp(cfg: Config):
 
             post_res = None
             if len(request.form["tree_data"]) > 0:
-                post_res = post_job(job, project_id, endpoint_path)
+                post_res = client.post_job(project_id, job)
 
             if (
                 isinstance(post_res, dict)
@@ -285,7 +160,7 @@ def construct_projects_bp(cfg: Config):
     @projects_bp.route("/<int:project_id>/jobs/<int:job_id>/csv")
     def projects_job_make_csv(project_id: int, job_id: int):  # type: ignore
         """Download results of a job as a csv-file."""
-        job = get_job(job_id, project_id, endpoint_path)
+        job = client.get_job(job_id, project_id)
         if job is None:
             return render_template("404.html"), 404
 
@@ -317,171 +192,6 @@ def construct_projects_bp(cfg: Config):
             )
 
     return projects_bp
-
-
-def get_project(project_id: int, endpoint: str) -> Optional[Project]:
-    """Get one project from endpoint.
-
-    Parameters
-    ----------
-    project_id  :   int
-                    Id of project to get.
-    endpoint    :   str
-                    API endpoint.
-
-    Returns
-    -------
-    Optional[Project]
-                Project from core with `project_id`.
-    """
-    result = handler.get(f"{endpoint}/projects/{project_id}")
-
-    if isinstance(result, requests.Response):
-        return Project.from_dict(result.json())  # type: ignore
-
-    return None
-
-
-def get_projects(endpoint: str) -> Optional[List[Project]]:
-    """Get all projects from endpoint.
-
-    Parameters
-    ----------
-    endpoint    :   str
-                    API endpoint.
-
-    Returns
-    -------
-    Optional[List[Project]]
-                List of all `Project` from `core`.
-    """
-    result = handler.get(f"{endpoint}/projects/")
-
-    if isinstance(result, requests.Response):
-        return [Project.from_dict(p) for p in result.json()]  # type: ignore
-
-    return None
-
-
-def post_project(project: Project, endpoint: str) -> None:
-    """Post project from endpoint.
-
-    Parameters
-    ----------
-    project     :   Project
-                    Project to send to `core` api.
-    endpoint    :   str
-                    API endpoint.
-    """
-    result = handler.post(f"{endpoint}/projects/", project.to_json())  # type: ignore
-
-    if isinstance(result, requests.Response):
-        logger.info(
-            "Project %s posted with status_code: %s",
-            project.id,
-            result.status_code,
-        )
-
-    return None
-
-
-def get_job(job_id: int, project_id: int, endpoint: str) -> Optional[Job]:
-    """Get job to job."""
-    try:
-        r_project = requests.get(f"{endpoint}/projects/{project_id}/")  # type: ignore
-        r_job = requests.get(f"{endpoint}/projects/{project_id}/jobs/{job_id}")  # type: ignore
-    except requests.ConnectionError:
-        logger.error("API is not running!")
-        return
-
-    if not r_project.status_code == requests.codes.ok:
-        print(f"Recived an err; {r_project.status_code}")
-        return
-
-    if not r_job.status_code == requests.codes.ok:
-        print(f"Recived an err; {r_job.status_code}")
-        return
-
-    return Job.from_dict(
-        r_job.json(), project_id, r_project.json()["name"]  # type: ignore
-    )
-
-
-def post_job(job: Job, project_id: int, endpoint: str):
-    """Post new job to job."""
-    try:
-        r = requests.post(  # type:ignore
-            f"{endpoint}/projects/{project_id}/jobs", data=job.to_json()
-        )
-    except requests.ConnectionError:
-        return "API is not running!"
-
-    if r.status_code == requests.codes.bad_request:
-        return r.json()["detail"]
-
-    if not r.status_code == requests.codes.created:
-        print(f"Recived an err; {r.status_code}")
-
-    return r.json()["id"]
-
-
-def change_job_status(
-    project_id: int, job_id: int, endpoint: str
-) -> Tuple[Optional[str], Optional[str]]:
-    """Change job status."""
-    try:
-        r_project = requests.get(f"{endpoint}/projects/{project_id}/")  # type: ignore
-        r_job = requests.get(f"{endpoint}/projects/{project_id}/jobs/{job_id}")  # type: ignore
-    except requests.ConnectionError:
-        logger.error("API is not running!")
-        return None, None
-
-    if not r_project.status_code == requests.codes.ok:
-        print(f"Recived an err; {r_project.status_code}")
-        return None, None  # type: ignore
-
-    if not r_job.status_code == requests.codes.ok:
-        print(f"Recived an err; {r_job.status_code}")
-        return None, None  # type: ignore
-
-    job = Job.from_dict(
-        r_job.json(), project_id, r_project.json()["name"]  # type: ignore
-    )
-
-    old_status = job.get_status()
-
-    new_status: str = ""
-
-    if job.get_status() == "pending" or job.get_status() == "paused":
-        r_post = requests.post(  # type:ignore
-            f"{endpoint}/projects/{project_id}/jobs/{job_id}/start"
-        )
-        if r_post.status_code == requests.codes.accepted:
-            new_status = "running"
-        else:
-            print(f"Recived an err; {r_post.status_code}")
-    elif job.get_status() == "running":
-        r_post = requests.post(  # type:ignore
-            f"{endpoint}/projects/{project_id}/jobs/{job_id}/pause"
-        )
-        if r_post.status_code == requests.codes.accepted:
-            new_status = "paused"
-        else:
-            print(f"Recived an err; {r_post.status_code}")
-    else:
-        new_status = "done"
-
-    return old_status, new_status  # type: ignore
-
-
-def check_api(endpoint: str) -> bool:
-    """Check if API responds."""
-    try:
-        requests.post(f"{endpoint}")  # type: ignore
-    except requests.ConnectionError:
-        return False
-
-    return True
 
 
 def path_to_dict(path: str) -> Dict[str, str]:
