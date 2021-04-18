3f08be79575b7602d0edf87c18eb133b51f1a773
Birger J. Nordølum <contact@mindtooth.no>
2021-04-18 13:15:29 +0200

    refactor: move to use css/js bundler

--- ui/ui/postcss.config.js
+++ ui/ui/postcss.config.js
@@ -1,5 +1,18 @@
+const path = require("path");
+
+const urlOptions = [
+  {
+    basePath: path.resolve(__dirname, "node_modules/jstree/src/themes/default"),
+    url: "copy",
+    assetsPath: path.resolve(__dirname, "static/dist/images"),
+    useHast: true,
+  },
+];
+
 module.exports = (ctx) => ({
   plugins: {
+    "postcss-import": {},
+    "postcss-url": urlOptions,
     tailwindcss: {},
     autoprefixer: {},
     cssnano:
