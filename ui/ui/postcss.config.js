507b9ada206658114d7b0ab6c4abcc468504417d
Birger J. Nordølum <contact@mindtooth.no>
2021-04-19 11:42:31 +0200

    refactor: make small fixes

--- ui/ui/postcss.config.js
+++ ui/ui/postcss.config.js
@@ -1,18 +1,17 @@
 const path = require("path");
 
-const urlOptions = [
-  {
-    basePath: path.resolve(__dirname, "node_modules/jstree/src/themes/default"),
-    url: "copy",
-    assetsPath: path.resolve(__dirname, "static/dist/images"),
-    useHast: true,
-  },
-];
-
 module.exports = (ctx) => ({
   plugins: {
     "postcss-import": {},
-    "postcss-url": urlOptions,
+    "postcss-url": {
+      basePath: path.resolve(
+        __dirname,
+        "node_modules/jstree/src/themes/default"
+      ),
+      url: "copy",
+      assetsPath: path.resolve(__dirname, "static/dist/images"),
+      useHast: true,
+    },
     tailwindcss: {},
     autoprefixer: {},
     cssnano:
