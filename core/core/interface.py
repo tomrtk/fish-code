af11346b73bca046343685a8a1b5962829de0724
Eirik Osland Lavik <eiriklav@stud.ntnu.no>
2021-04-20 15:19:06 +0200

    refactor(core::interface): use opencv
    Using OpenCV to convert image to bytes. This is significantly faster by
    about a factor of 10.
    
    Attempts were made to speed this up with multiprocessing, however it
    made it riddled with bugs, and it didn't have a significant increase in
    speed.

--- core/core/interface.py
+++ core/core/interface.py
@@ -1,13 +1,12 @@
 """Interface module for communicating with other packages like `Tracing`."""
 import io
 import logging
-from dataclasses import asdict, dataclass
-from datetime import datetime
-from typing import Dict, List, Optional
+from dataclasses import dataclass
+from typing import List, Tuple
 
+import cv2 as cv
 import numpy as np
 import requests
-from PIL import Image
 
 import core.model
 
@@ -158,17 +157,9 @@ class Detector:  # pragma: no cover
 
         # if a single image
         if frames.ndim == 3:
-            byte_io = io.BytesIO()
-            Image.fromarray(frames).save(byte_io, "png")
-            byte_io.seek(0)
-            byte_frames = [("images", byte_io)]
+            byte_frames = [("images", img_to_byte(frames))]
         else:
-            byte_frames = list()
-            for i in range(frames.shape[0]):
-                byte_io = io.BytesIO()
-                Image.fromarray(frames[i, ...]).save(byte_io, "png")
-                byte_io.seek(0)
-                byte_frames.append(("images", byte_io))
+            byte_frames = [("images", img_to_byte(img)) for img in frames]
 
         response = requests.post(
             f"http://{self.host}:{self.port}/predictions/{model_name}/",
@@ -233,3 +224,25 @@ class Detector:  # pragma: no cover
         )
 
         return list()
+
+
+def img_to_byte(img: np.ndarray) -> io.BytesIO:
+    """Convert image as np.ndarray to image byte.
+
+    This tried to do as few check to keep it as fast as possible.
+
+    Parameters
+    ----------
+    img : np.ndarray
+        imagedata
+
+    Return
+    ------
+    io.BytesIO
+        Image data as byte.
+    """
+    img = cv.cvtColor(img, cv.COLOR_BGR2RGB)  # type: ignore
+    retval, img_byte = cv.imencode(".png", img)  # type: ignore
+    if not retval:
+        raise RuntimeError("shit went haywire")
+    return io.BytesIO(img_byte)
