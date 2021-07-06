7623f1b1c5192081c510b478823ee1ff96d3f0ec
kad1n <thekad1nat@gmail.com>
2021-07-06 20:37:18 +0200

    test: check parameter override to detection and tracing

--- tests/unit/detection/test_main.py
+++ tests/unit/detection/test_main.py
@@ -22,3 +22,19 @@ def test_main_debug(caplog):
         assert (
             caplog.records[0].getMessage() == "Detection started in debug mode"
         )
+
+
+def test_main_host_port_override(caplog):
+    """Happy case test of main with overridden port and host."""
+    with caplog.at_level(logging.DEBUG):
+        main(["--test", "--host", "1.2.3.4", "--port", "1337"])
+
+        assert (
+            caplog.records[0].getMessage()
+            == "Overriding detection API hostname from 127.0.0.1 to 1.2.3.4"
+        )
+        assert (
+            caplog.records[1].getMessage()
+            == "Overriding detection API port from 8003 to 1337"
+        )
+        assert caplog.records[2].getMessage() == "Detection started"
