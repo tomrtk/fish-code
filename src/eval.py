0c50edd9cb4226f412109a49da1e8c74fc9b0e9a
R0flcopt3r <12752060+R0flcopt3r@users.noreply.github.com>
2021-07-08 10:29:28 +0200

    chore: lots of stuff

--- src/eval.py
+++ src/eval.py
@@ -9,6 +9,17 @@ from typing import List, Optional, Dict
 from core import model, services
 from datetime import datetime
 import torch
+from PIL import Image
+from io import BytesIO
+from core import utils
+
+
+@dataclass
+class Frame:
+    idx: int
+    detections: List[tracker.Detection]
+    timestamp: Optional[datetime] = None
+    video_id: Optional[int] = None
 
 
 detection.model["fishy"] = (  # type: ignore
@@ -56,57 +67,60 @@ detection.label["fishy2"] = [
 imgs: List[np.ndarray] = [np.zeros((640, 640, 3))]
 track = tracker.SortTracker()
 
-
 vl = services.VideoLoader(
     [
         model.Video.from_path(
             "/home/eirik/Downloads/Abbor mørkt-middels-dårlige forhold-cut-[2020-03-28_12-30-10].mp4"
         )
     ],
-    batchsize=450,
+    batchsize=50,
 )
 
 from_detect: Dict[int, List[detection.schema.Detection]] = dict()
+tracked = list()
 
-for batch_nr, batch in vl.generate_batches():
+for batchnr, (
+    progress,
+    batch,
+    timestamp,
+    video_for_frame,
+    framenumbers,
+) in vl.generate_batches():
+
+    # Trying to replicate production code
+    imgs = [np.array(Image.open(utils.img_to_byte(img))) for img in batch]
 
     from_detect = detection.detect(
-        batch[1],  # type: ignore
+        imgs,  # type: ignore
         detection.model["fishy"][0],
         detection.model["fishy"][0],
     )
-    print(f"{batch_nr}/{vl._total_batches}")
+    print(f"{batchnr}/{vl._total_batches}")
 
+    result: List[Frame] = []
+    for frame_no, detections in from_detect.items():
+        if len(detections) == 0:
+            result.append(Frame(frame_no, []))
+        else:
+            result.append(
+                frame_no,
+                [
+                    tracker.Detection(
+                        tracker.BBox(
+                            detection.x1,
+                            detection.y1,
+                            detection.x2,
+                            detection.y2,
+                        ),
+                        probability=detection.confidence,
+                        label=detection.label,
+                        frame=frame_no,
+                        frame_id=None,
+                        video_id=None,
+                    )
+                    for detection in detections
+                ],
+            )
 
-@dataclass
-class Frame:
-    idx: int
-    detections: List[tracker.Detection]
-    timestamp: Optional[datetime] = None
-    video_id: Optional[int] = None
-
-
-result: List[Frame] = []
-for frame_no, detections in from_detect.items():
-    if len(detections) == 0:
-        result.append(Frame(frame_no, []))
-    else:
-        result.append(
-            frame_no,
-            [
-                tracker.Detection(
-                    tracker.BBox(
-                        detection.x1, detection.y1, detection.x2, detection.y2
-                    ),
-                    probability=detection.confidence,
-                    label=detection.label,
-                    frame=frame_no,
-                    frame_id=None,
-                    video_id=None,
-                )
-                for detection in detections
-            ],
-        )
-
-for frame in result:
-    track.update(frame.detections)
+    for frame in result:
+        tracked = track.update(frame.detections)
