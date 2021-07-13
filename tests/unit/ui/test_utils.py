291c193034d986b64790bd04392e46d29b985495
tomrtk <43438127+tomrtk@users.noreply.github.com>
2021-07-13 11:04:36 +0200

    fix: typo in code and docstring
    Co-authored-by: Birger J. Nordølum <contact@mindtooth.no>

--- tests/unit/ui/test_utils.py
+++ tests/unit/ui/test_utils.py
@@ -5,7 +5,7 @@ from pydantic.error_wrappers import ValidationError
 from ui.projects.utils import validate_int, SQLITE_INTEGER_LIMIT
 
 
-def test_vaidate_int() -> None:
+def test_validate_int() -> None:
     """Test int validation."""
     assert validate_int(0) == 0
     assert validate_int(-(2 ** 64)) is None
