5de9396fabf795efc96e355229770861a2c45392
Birger J. Nordølum <contact@mindtooth.no>
2022-06-03 17:23:39 +0200

    chore: sync with new npm packages
    Because we distribute the compiled source files.

--- src/ui/tailwind.config.js
+++ src/ui/tailwind.config.js
@@ -2,7 +2,7 @@ const colors = require("tailwindcss/colors");
 
 module.exports = {
   mode: "jit",
-  purge: {
+  content: {
     content: [
       "./templates/**/*.html",
       "./projects/templates/**/*.html",
